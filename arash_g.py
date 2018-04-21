b8bf2751cab6657a2f93dd99d2edd7a2b0c1156d
arashi bahrami <arash.b7@gmail.com>
2018-04-21 17:27:03 +0900

    did a lot

--- arash_g.py
+++ arash_g.py
@@ -1,74 +1,102 @@
 from random import randint
 import os
-class Player:
-   def __init__(self, view_width, view_height):
-       self.x = 0
-       self.y = 0
-       self.charactar = "X"
-       self.view_width = view_height - 1
-       self.view_height = view_height - 1
-       self.last_pos = (0, 0)
+from time import sleep
+import numpy
+from baseclasses import gameobject, movingobject
 
-   def move(self, direction):
-       self.last_pos = (self.x, self.y)
-       if direction.lower() in ["left", 'a'] and self.x > 0:
-           self.x -= 1
-       elif direction.lower() in ["right", 'd'] and self.x < self.view_width:
-           self.x += 1
-       elif direction.lower() in ["up", 'w'] and self.y < self.view_height:
-           self.y += 1
-       elif direction.lower() in ["down", 's'] and self.y > 0:
-           self.y -= 1
+
+class Wall(GameObject):
+    def __init__(self, view_width: int, view_height: int) -> None:
+        super().__init__()
+        self.view_width = view_height - 1
+        self.view_height = view_height - 1
+        self.last_pos = (0, 0)
+        self._damage = randint(0,1)
+        self._health = 10
+        self.charactar: str = "#"
+
+class Player(movingobject):
+    def __init__(self, view_width: int, view_height: int) -> None:
+        super().__init__()
+        self.view_width = view_height - 1
+        self.view_height = view_height - 1
+        self.last_pos = (0, 0)
+        self.damage = randint(10, 45)
+        self.health = 100
+        self.charactar: str = "X"
 
 class GameView:
-   def __init__(self, width, height):
-       self.width = width
-       self.height = height
-       self.message = ""
 
-   def render(self, map_frame):
-      canvas = []
-      for y in map_frame:
-          row = []
-          for x in y:
-              if x == 0:
-                  row.append(" ")
-              elif x == 1:
-                  row.append("#")
-              else:
-                  row.append(x)
-          canvas.append("".join(row))
-      canvas.reverse()
-      os.system('cls' if os.name == 'nt' else 'clear')
-      print("\n".join(canvas))
-      print(self.message)
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.message = ""
+
+    def render(self, map_frame, wall):
+        canvas = []
+        for y in map_frame:
+            row = []
+            for x in y:
+                if x == 0:
+                    row.append(" ")
+                elif x == 1:
+                    row.append(wall.charactar)
+                else:
+                    row.append(x)
+            canvas.append("".join(row))
+        canvas.reverse()
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n".join(canvas))
+        print(self.message)
 
 
 class Game:
-   def __init__(self):
-       self.view_width = 16
-       self.view_height = 16
-       self.game_map = [[randint(0,randint(0,randint(0,1))) for _ in range(self.view_width)] for _ in range(self.view_height)]
-       self.view = GameView(self.view_width, self.view_height)
-       self.player = Player(self.view_width, self.view_height)
-       self.update_player_pos()
-       self.view.render(self.game_map)
 
-   def update_player_pos(self):
-       if self.game_map[self.player.y][self.player.x] == 0:
-           self.game_map[self.player.last_pos[1]][self.player.last_pos[0]] = 0
-           self.game_map[self.player.y][self.player.x] = self.player.charactar
-       else:
-           self.player.x = self.player.last_pos[0]
-           self.player.y = self.player.last_pos[1]
-           self.view.message = f"wall ahead turn some other direction {self.game_map[self.player.y][self.player.x]}"
+    def __init__(self):
+        self.view_width = 16
+        self.view_height = 16
+        self.game_map = np.random.rand(0,1,(self.view_height,self.view_width))#[[randint(0,randint(0,randint(0,1))) for _ in range(self.view_width)] for _ in range(self.view_height)]
+        self.view = GameView(self.view_width, self.view_height)
+        self.player = Player(self.view_width, self.view_height)
+        self.wall = Wall(self.view_width, self.view_height)
+        self.update_player_pos()
+        self.view.render(self.game_map, self.wall)
 
-   def run(self):
-       while True:
-           direction = input("move to: ")
-           self.player.move(direction)
-           self.update_player_pos()
-           self.view.render(self.game_map)
+    def update_player_pos(self):
+        if self.game_map[self.player.y][self.player.x] == 0:
+            self.game_map[self.player.last_pos[1]][self.player.last_pos[0]] = 0
+            self.game_map[self.player.y][self.player.x] = self.player.charactar
+        else:
+            self.player.x = self.player.last_pos[0]
+            self.player.y = self.player.last_pos[1]
+            self.view.message = f"wall ahead turn some other direction {self.game_map[self.player.y][self.player.x]}"
+            self.battle(player=self.player, enemy=self.wall)
+
+    def run(self):
+        while True:
+            direction = input("move to: ")
+            self.player.move(direction)
+            self.update_player_pos()
+            self.view.render(self.game_map, self.wall)
+
+    def battle(self, player, enemy, auto=False) -> bool:
+        while not (player.dead == True or enemy.dead == True):
+            if auto:
+                player.attack(enemy)
+                enemy.attack(player)
+            else:
+                player_input = input("what to do?(attack / run / use magic(not usable))")
+                if player_input == "attack":
+                    player.attack(enemy)
+                    print(player.health, enemy.dead)
+                    enemy.attack(player)
+                    print(player.health, enemy.health)
+                elif player_input == "run":
+                    print(player.health, enemy.health)
+                    return False
+                else:
+                    print(player_input)
+                    continue
 
 main = Game()
 main.run()
